4928e8710bfd4b5f9a7ce2def45a76a2d98f07be
Tanay Tadas <tanaysid97@gmail.com>
2022-05-02 16:57:17 -0400

    changes in react version
    made changes from react 17 to 18 and restructured folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,44 @@
-import React from "react";
-import "./App.css";
-import logo from "./img/books.jpeg";
-import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
-import BookDetails from "./components/BooksDetails";
-import NewAdditions from "./components/NewAdditions";
-import Library from "./components/Library";
+import React from 'react';
+import './App.css';
+import logo from './img/books.jpeg';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import BookDetails from './components/BooksDetails';
+import NewAdditions from './components/NewAdditions';
+import Library from './components/Library';
 
 const App = () => {
     return (
         <Router>
-            <div className='App'>
-                <header className='App-header'>
+            <div className="App">
+                <header className="App-header">
                     {/* <img src={logo} className='App-logo' alt='logo' /> */}
-                    <h1 className='App-title'>
+                    <h1 className="App-title">
                         Welcome to the House of books.
                     </h1>
                     <Link
-                        className='showlink'
-                        to='/books/626ea903aa3f53db40cc8f99'
+                        className="showlink"
+                        to="/books/626ea903aa3f53db40cc8f99"
                     >
                         Books
                     </Link>
-                    <Link className='showlink' to='/books/newAdditions'>
+                    <Link className="showlink" to="/books/newAdditions">
                         New Additions
                     </Link>
-                    <Link className='showlink' to='/library'>
+                    <Link className="showlink" to="/library">
                         Library
                     </Link>
                 </header>
                 <br />
                 <br />
-                <div className='App-body'>
+                <div className="App-body">
                     <Routes>
                         {/* <Route path='/' element={<Home />} /> */}
-                        <Route path='/books/:id' element={<BookDetails />} />
+                        <Route path="/books/:id" element={<BookDetails />} />
                         <Route
-                            path='/books/newAdditions'
+                            path="/books/newAdditions"
                             element={<NewAdditions />}
                         />
-                        <Route path='/library' element={<Library />} />
+                        <Route path="/library" element={<Library />} />
                     </Routes>
                 </div>
             </div>
