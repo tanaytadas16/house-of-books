8cfaeda126472e0d1689d77f31058342ecd46d97
VaidyaHarshal <harshal.vaidya300@gmail.com>
2022-05-03 01:49:13 -0400

    Most popular books and Redis for recently viewed books changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 // import logo from "./img/books.jpeg";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import BookDetails from "./components/BooksDetails";
-import NewAdditions from "./components/NewAdditions";
-import BooksList from "./components/BooksList";
-import Home from "./components/Home";
-import Library from "./components/Library";
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import BookDetails from './components/BooksDetails';
+import NewAdditions from './components/NewAdditions';
+import BooksList from './components/BooksList';
+import Home from './components/Home';
+import Library from './components/Library';
+import MostPopular from './components/MostPopular';
+import RecentBooks from './components/RecentBooks';
 
 const App = () => {
   return (
@@ -24,16 +26,28 @@ const App = () => {
           <Link className="showlink" to="/library">
             Library
           </Link>
+          <Link className="showlink" to="/books/mostPopular">
+            Popular Books
+          </Link>
+          <Link className="showlink" to="/books/recents">
+            Recently Viewed
+          </Link>
         </header>
         <br />
         <br />
         <div className="App-body">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<BooksList />} />
-            <Route path="/books/:id" element={<BookDetails />} />
-            <Route path="/books/newAdditions" element={<NewAdditions />} />
-            <Route path="/library" element={<Library />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/books" element={<BooksList />} />
+            <Route exact path="/books/:id" element={<BookDetails />} />
+            <Route
+              exact
+              path="/books/newAdditions"
+              element={<NewAdditions />}
+            />
+            <Route exact path="/library" element={<Library />} />
+            <Route exact path="/books/mostPopular" element={<MostPopular />} />
+            <Route exact path="/books/recents" element={<RecentBooks />} />
           </Routes>
         </div>
       </div>
--- client/src/components/RecentBooks.js
+++ client/src/components/RecentBooks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import noImage from '../img/download.jpeg';
 import {
   makeStyles,
@@ -45,17 +45,17 @@ const useStyles = makeStyles({
   },
 });
 
-const BooksList = () => {
+const RecentBooks = () => {
   const [loading, setLoading] = useState(true);
   const classes = useStyles();
   const [bookDetailsData, setBookDetailsData] = useState(undefined);
   let card = null;
-  const history = useNavigate();
-
   useEffect(() => {
+    console.log('useEffect fired');
     async function fetchData() {
       try {
-        const url = `http://localhost:4000/books`;
+        console.log('Before axios call');
+        const url = `http://localhost:4000/books/recents`;
         const { data } = await axios.get(url);
         console.log(data);
         setBookDetailsData(data);
@@ -66,41 +66,9 @@ const BooksList = () => {
     }
     fetchData();
   }, []);
-
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-  function formatDate(date) {
-    return [
-      padTo2Digits(date.getMonth() + 1),
-      padTo2Digits(date.getDate()),
-      date.getFullYear(),
-    ].join('-');
-  }
-
-  const buyBook = (customerId, bookId, quantity, price) => {
-    let todayDate = formatDate(new Date());
-    console.log(todayDate);
-    console.log(customerId, bookId);
-    let dataBody = {
-      customerId: customerId,
-      bookId: bookId,
-      quantity: quantity,
-      totalPrice: quantity * price,
-    };
-    axios
-      .post('http://localhost:4000/books/purchase', {
-        data: dataBody,
-      })
-      .then(function (response) {
-        console.log(response.data);
-        history('/', { replace: true }); //to be changed to cart
-      });
-  };
-
   const buildCard = (book) => {
     return (
-      <Grid item xs={10} sm={7} md={5} lg={4} xl={3} height={45} key={book._id}>
+      <Grid item xs={10} sm={7} md={5} lg={4} xl={3} key={book._id}>
         <Card className={classes.card} variant="outlined">
           <CardActionArea>
             <Link to={`/books/${book._id}`}>
@@ -130,7 +98,7 @@ const BooksList = () => {
                     <p>
                       <dt className="title">Price:</dt>
                       {book && book.price ? (
-                        <dd>{book.price}</dd>
+                        <dd>$ {book.price}</dd>
                       ) : (
                         <dd>N/A</dd>
                       )}
@@ -140,15 +108,6 @@ const BooksList = () => {
               </CardContent>
             </Link>
           </CardActionArea>
-          <button
-            type="button"
-            className="button"
-            onClick={() =>
-              buyBook('626f260f12231eb05560c6b7', book._id, 2, book.price)
-            }
-          >
-            Buy
-          </button>
         </Card>
       </Grid>
     );
@@ -176,4 +135,4 @@ const BooksList = () => {
   }
 };
 
-export default BooksList;
+export default RecentBooks;
