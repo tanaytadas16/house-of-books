1b3784259abb5ead9a4e9dc545f273295188fdfc
Sushant <sush1393@gmail.com>
2022-05-08 20:09:17 -0400

    reverting api changes

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,22 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCZlDJH6SBKpd7Q4HU0dXXVh9gR0GdJUoM",
-    authDomain: "house-of-books-fc08c.firebaseapp.com",
-    projectId: "house-of-books-fc08c",
-    storageBucket: "house-of-books-fc08c.appspot.com",
-    messagingSenderId: "827562959146",
-    appId: "1:827562959146:web:0d32bcff164c2e0c76be53"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 initializeApp(firebaseConfig);
