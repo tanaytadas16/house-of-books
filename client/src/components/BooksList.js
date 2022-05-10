e09ab3c21866d533f6477560704b05e3a69b7786
tanaytadas16 <tanaysid97@gmail.com>
2022-05-09 23:21:41 -0400

    added changes for heroku and netlify

--- client/src/components/BooksList.js
+++ client/src/components/BooksList.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import noImage from "../assets/images/no-image.jpeg";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import noImage from '../assets/images/no-image.jpeg';
 import {
     makeStyles,
     Card,
@@ -9,37 +9,37 @@ import {
     Grid,
     CardContent,
     CardMedia,
-    Typography
-} from "@material-ui/core";
+    Typography,
+} from '@material-ui/core';
 const useStyles = makeStyles({
     card: {
         maxWidth: 550,
-        height: "100%",
-        marginLeft: "auto",
-        marginRight: "auto",
+        height: '100%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
         borderRadius: 5,
-        border: "1px solid #222",
+        border: '1px solid #222',
         boxShadow:
-            "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);",
-        color: "#222",
+            '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);',
+        color: '#222',
     },
     titleHead: {
-        borderBottom: "1px solid #222",
-        fontWeight: "bold",
-        color: "#222",
-        fontSize: "large",
+        borderBottom: '1px solid #222',
+        fontWeight: 'bold',
+        color: '#222',
+        fontSize: 'large',
     },
     grid: {
         flexGrow: 1,
-        flexDirection: "row",
+        flexDirection: 'row',
     },
     media: {
-        height: "100%",
-        width: "100%",
+        height: '100%',
+        width: '100%',
     },
     button: {
-        color: "#222",
-        fontWeight: "bold",
+        color: '#222',
+        fontWeight: 'bold',
         fontSize: 12,
     },
 });
@@ -54,7 +54,7 @@ const BooksList = () => {
     useEffect(() => {
         async function fetchData() {
             try {
-                const url = `http://localhost:4000/books`;
+                const url = `https://houseof-books.herokuapp.com/books`;
                 const { data } = await axios.get(url);
                 console.log(data);
                 setBookDetailsData(data);
@@ -67,14 +67,14 @@ const BooksList = () => {
     }, []);
 
     function padTo2Digits(num) {
-        return num.toString().padStart(2, "0");
+        return num.toString().padStart(2, '0');
     }
     function formatDate(date) {
         return [
             padTo2Digits(date.getMonth() + 1),
             padTo2Digits(date.getDate()),
             date.getFullYear(),
-        ].join("-");
+        ].join('-');
     }
 
     const buyBook = (customerId, bookId, quantity, price) => {
@@ -88,12 +88,12 @@ const BooksList = () => {
             totalPrice: quantity * price,
         };
         axios
-            .post("http://localhost:4000/books/purchase", {
+            .post('http://localhost:4000/books/purchase', {
                 data: dataBody,
             })
             .then(function (response) {
                 console.log(response.data);
-                history("/", { replace: true }); //to be changed to cart
+                history('/', { replace: true }); //to be changed to cart
             });
     };
 
@@ -109,26 +109,26 @@ const BooksList = () => {
                 height={45}
                 key={book._id}
             >
-                <Card className={classes.card} variant='outlined'>
+                <Card className={classes.card} variant="outlined">
                     <CardActionArea>
                         <Link to={`/books/${book._id}`}>
                             <CardMedia
                                 className={classes.media}
-                                component='img'
+                                component="img"
                                 image={book.url ? book.url : noImage}
-                                title='book image'
+                                title="book image"
                             />
 
                             <CardContent>
                                 <Typography
-                                    variant='body2'
-                                    color='textSecondary'
-                                    component='span'
+                                    variant="body2"
+                                    color="textSecondary"
+                                    component="span"
                                 >
-                                    <p className='title1'>{book.title}</p>
+                                    <p className="title1">{book.title}</p>
                                     <dl>
                                         <p>
-                                            <dt className='title'>Genre:</dt>
+                                            <dt className="title">Genre:</dt>
                                             {book && book.genre ? (
                                                 <dd>{book.genre}</dd>
                                             ) : (
@@ -136,7 +136,7 @@ const BooksList = () => {
                                             )}
                                         </p>
                                         <p>
-                                            <dt className='title'>Price:</dt>
+                                            <dt className="title">Price:</dt>
                                             {book && book.price ? (
                                                 <dd>{book.price}</dd>
                                             ) : (
@@ -149,11 +149,11 @@ const BooksList = () => {
                         </Link>
                     </CardActionArea>
                     <button
-                        type='button'
-                        className='button'
+                        type="button"
+                        className="button"
                         onClick={() =>
                             buyBook(
-                                "627161da17f0455539944549",
+                                '627161da17f0455539944549',
                                 book._id,
                                 2,
                                 book.price
