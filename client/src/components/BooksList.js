f6805f6878f868279fb5e2dadac2c5dee7085a5e
bhatsushant <sush1393@gmail.com>
2022-05-10 00:49:02 -0400

    Update BooksList.js

--- client/src/components/BooksList.js
+++ client/src/components/BooksList.js
@@ -54,7 +54,7 @@ const BooksList = () => {
     useEffect(() => {
         async function fetchData() {
             try {
-                const url = `httpss://houseof-books.herokuapp.com/books`;
+                const url = `https://houseof-books.herokuapp.com/books`;
                 const { data } = await axios.get(url);
                 console.log(data);
                 setBookDetailsData(data);
