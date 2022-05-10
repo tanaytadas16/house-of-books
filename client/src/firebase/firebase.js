934eb678b557290aa1ccffe9fcb1cb72aef7a7c2
Sushant <sush1393@gmail.com>
2022-05-09 23:48:31 -0400

    Revert "reverting api changes"
    This reverts commit cd18ab3338d977d988e715bd290a5ffb608b7cf9.

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,21 +1,21 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged,
-} from 'firebase/auth';
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCZlDJH6SBKpd7Q4HU0dXXVh9gR0GdJUoM',
-    authDomain: 'house-of-books-fc08c.firebaseapp.com',
-    projectId: 'house-of-books-fc08c.firebaseapp.com',
-    storageBucket: 'house-of-books-fc08c.appspot.com',
-    messagingSenderId: '827562959146',
-    appId: '1:827562959146:web:0d32bcff164c2e0c76be53',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
@@ -24,24 +24,24 @@ export const auth = getAuth();
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt: 'select_account',
+  prompt: "select_account",
 });
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const createNativeUser = async (email, password) => {
-    if (!email || !password) return;
+  if (!email || !password) return;
 
-    return await createUserWithEmailAndPassword(auth, email, password);
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
 
 export const NativeSignIn = async (email, password) => {
-    if (!email || !password) return;
+  if (!email || !password) return;
 
-    return await signInWithEmailAndPassword(auth, email, password);
+  return await signInWithEmailAndPassword(auth, email, password);
 };
 
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
-    onAuthStateChanged(auth, callback);
+  onAuthStateChanged(auth, callback);
