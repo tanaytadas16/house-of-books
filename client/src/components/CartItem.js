fbd31100cd66cab3d5503be55d01a2eeb0260b6e
Sushant <sush1393@gmail.com>
2022-05-11 17:06:44 -0400

    add cart component

---
+++ client/src/components/CartItem.js
@@ -0,0 +1,18 @@
+import '../styles/CartItem.scss';
+
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span>{name}</span>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,56 +1,64 @@
 import React, { useContext } from 'react';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link } from 'react-router-dom';
 import { UserContext } from '../contexts/userContext';
 import { signOutUser } from '../firebase/firebase';
+import { useSelector } from 'react-redux';
+import CartIcon from './CartIcon';
+import CartDropdown from './CartDropdown';
+import { selectIsCartOpen } from '../store/selector/cartSelector';
 import { ReactComponent as OpenBookLogo } from '../assets/images/openbook.svg';
 import '../styles/Navigation.scss';
 
 const Navigation = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  console.log(currentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
-    const signOutHandler = async () => {
-        await signOutUser();
-        setCurrentUser(null);
-    }
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  };
 
-    return (
-        <>
-            <div className="navigation">
-                <Link className='logo-container' to='/'>
-                    <OpenBookLogo className="logo" /> <span>HOUSE OF BOOKS</span>
-                </Link>
-                <div className="navigation">
-                    <div className="nav-links-container">
-                        <Link className="nav-link" to="/books">
-                            BOOKS
-                        </Link>
-                        <Link className="nav-link" to="/books/newAdditions">
-                            NEW ADDITIONS
-                        </Link>
-                        <Link className="nav-link" to="/library">
-                            LIBRARY
-                        </Link>
-                        <Link className="nav-link" to="/books/mostPopular">
-                            POPULAR BOOKS
-                        </Link>
-                        <Link className="nav-link" to="/books/recents">
-                            RECENTLY VIEWED
-                        </Link>
-                        {
-                            currentUser ?
-                                (
-                                    <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
-                                ) :
-                                (
-                                    <Link className="nav-link" to='/auth'>SIGN IN</Link>
-                                )
-                        }
-                    </div>
-                </div>
-            </div>
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <OpenBookLogo className="logo" /> <span>HOUSE OF BOOKS</span>
+        </Link>
+        <div className="navigation">
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/books">
+              BOOKS
+            </Link>
+            <Link className="nav-link" to="/books/newAdditions">
+              NEW ADDITIONS
+            </Link>
+            <Link className="nav-link" to="/library">
+              LIBRARY
+            </Link>
+            <Link className="nav-link" to="/books/mostPopular">
+              POPULAR BOOKS
+            </Link>
+            {/* <Link className="nav-link" to="/books/recents">
+              RECENTLY VIEWED
+            </Link> */}
+            {currentUser ? (
+              <span className="nav-link" onClick={signOutHandler}>
+                SIGN OUT
+              </span>
+            ) : (
+              <Link className="nav-link" to="/auth">
+                SIGN IN
+              </Link>
+            )}
+            <CartIcon />
+          </div>
+          {isCartOpen && <CartDropdown />}
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
 
 export default Navigation;
