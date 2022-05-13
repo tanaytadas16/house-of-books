4d56d22581c9ffcb1c5d0f8bbe320b4ee694c162
Sushant <sush1393@gmail.com>
2022-05-13 04:10:54 -0400

    add cart icon

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -58,6 +58,7 @@ const Navigation = () => {
                 PROFILE
               </Link>
             ) : null}
+            <CartIcon />
           </div>
           {isCartOpen && <CartDropdown />}
         </div>
