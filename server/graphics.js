194dd9d44091d08a4062d444ef58f1dcb7c61972
Anchal Bansal <anchalsingla20@gmail.com>
2022-05-14 14:34:41 -0400

    GraphicsMagick changes

--- server/graphics.js
+++ server/graphics.js
@@ -1,14 +1,14 @@
 var fs = require("fs"),
     gm = require("gm");
 
-var dir = __dirname + "/imgs";
-function createImage(text) {
+var dir = __dirname + "/imgs/";
+function createImage(text, email) {
     gm(150, 100, "#84defaaa")
         .fontSize(68)
         .stroke("#efe", 3)
         .fill("#000000")
         .drawText(20, 72, text)
-        .write(dir + "/initials.png", function (err) {
+        .write(dir + email + ".png", function (err) {
             if (err) return console.dir(arguments);
             console.log(this.outname + " created  :: " + arguments[3]);
         });
